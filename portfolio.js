62a79f9002a2e510e1d1cc7c9a6f40da7e35de88
Lrodas01 <lrodaroda23@gmail.com>
2024-06-26 08:16:08 -0700

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+//Make 3 seperate function for your challenges
+
+//This is for Slack
+
+document.getElementById ('slackChallenge').addEventListener ("click", function selectSlack () {document.getElementById ('changeText').innerText = "I plan on using Slack to communicate with my teammates to discuss potential products. I would be able to learn from my peers and ask for their advice. I will utilize Slack to transfer files, as well as Google Drives, and share ideas with my teammates. I would use Slack to communicate with professionals in the industry so that I can learn what they do. I would also take the opportunity to ask about their work expriences so I know what to expect when I work in the industry."
+})
+// Challenge Name
+document.getElementById ('slackChallenge').addEventListener ("click", function selectSlack () {document.getElementById ('ChallengeName').innerText = "  ‎ ‎ ‎ ‎ ‎Slack Challenge"
+})
+//Date
+document.getElementById ('slackChallenge').addEventListener ("click", function selectText () {document.getElementById ('changeDate').innerText = "  ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎  06/06/2024"
+})
+
+//This is now for the githubChallenge
+
+document.getElementById ('githubChallenge').addEventListener ("click", function selectSlack () {document.getElementById ('changeText').innerText = "From what I found, git is a version control system, which records any changes to your code and can be found in the repository. This repository is a library of any change you make to you code and even keeps a track who made said changes. Specifically, git is a distributed version control system which gives your teammates a copy of code and its changes. This is useful when you need to revert to an earlier version of your code without having to store your project in multiple folders or having to wait for the central server to go online. This makes git very popular among professionals as you can easily revert changes, collaborate with your teammates, and is free. Github is a paid service that runs the git yet it makes a centralized version control system, allowing you to access your code and history at any moment, and, im guessing, since it's a git, it should store code locally as well. Github also provides some sort of security to your code and also makes collaboration easier."
+})
+// Challenge Name
+document.getElementById ('githubChallenge').addEventListener ("click", function selectSlack () {document.getElementById ('ChallengeName').innerText = "   ‎ ‎ ‎ ‎ Github Challenge"
+})
+//Date
+document.getElementById ('githubChallenge').addEventListener ("click", function selectText () {document.getElementById ('changeDate').innerText = "  ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎  06/08/2024"
+})
+
+// This is for the Leadership Challenge
+
+document.getElementById ('leadershipChallenge').addEventListener ("click", function selectSlack () {document.getElementById ('changeText').innerText = "In my community, I have practiced being a role model by showing patience and kindness. Especially when I have a younger brother since he can be a bit annoying to take care of, but I always try my best to be kind to him and being patient. Being a role model to young black and Latino means that I am responsible for showing the right way to act when confronted negatively or positively. Last week, I was able to help a TxT brother by guiding them in their Light bulb challenge. Although I am not very knowledgeable in coding, I tried my best to explain how to do things so that they can understand a bit on what they are doing. One action I believe that lead by example was when I encouraged my teammates not to feel bad after losing. Since my team had no knowledge on how to play the game, we suffered a ton of losses, which took a toll on their morale. But to me, losing was a learning experience, and so I told this to my team. From then, we would reflect on out games to acknowledge our mistakes and learn to avoid that same mistake."
+})
+// Challenge Name
+document.getElementById ('leadershipChallenge').addEventListener ("click", function selectSlack () {document.getElementById ('ChallengeName').innerText = "  ‎ ‎ ‎ ‎ Leadership Challenge"
+})
+//Date
+document.getElementById ('leadershipChallenge').addEventListener ("click", function selectText () {document.getElementById ('changeDate').innerText = "  ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ 06/13/2024"
+})
+
